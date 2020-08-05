039422bdfa340a47279e111b24b63c936decb390
Hong10 <alexeyhong10@gmail.com>
2020-08-05 23:20:16 +0500

    vue.config.js updated.

--- vue.config.js
+++ vue.config.js
@@ -7,8 +7,8 @@ module.exports = {
 
   pluginOptions: {
     i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
+      locale: process.env.VUE_APP_I18N_LOCALE,
+      fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE,
       localeDir: 'locales',
       enableInSFC: false,
     },
