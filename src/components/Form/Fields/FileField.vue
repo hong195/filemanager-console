af6545c510fd8b5c7244b0c13884234d9585091d
saodat <saodat.9808@gmail.com>
2020-12-18 13:16:25 +0500

    File field: multiple files and read

--- src/components/Form/Fields/FileField.vue
+++ src/components/Form/Fields/FileField.vue
@@ -1,12 +1,49 @@
 <template>
-  <validation-provider
-    v-slot="{ errors }"
-    tag="div"
-    :rules="validationRule"
-    :name="label"
-  >
-    <v-file-input v-model="innerValue" />
-  </validation-provider>
+  <div>
+    <ul class="files">
+      <li
+        v-for="(item, key) in files"
+        :key="item.id"
+        class="mb-3 files__single"
+        :style="{background: getIcon(key).color}"
+      >
+        {{ item.name ? item.name : item.file_name }}
+
+        <v-icon
+          small
+          class="close"
+          color="white"
+        >
+          mdi-close
+        </v-icon>
+        <v-icon
+          dark
+        >
+          {{ getIcon(key).icon }}
+        </v-icon>
+        {{ sizeFormatter(item.size) }}
+      </li>
+    </ul>
+    <validation-provider
+      v-slot="{ errors }"
+      tag="div"
+      :rules="validationRule"
+      :name="label"
+    >
+      <div
+        class="file-input"
+        @click="onChange"
+      >
+        <v-file-input
+          ref="file1"
+          v-model="innerValue"
+          multiple
+          hide-input
+        />
+        Выберите файлы
+      </div>
+    </validation-provider>
+  </div>
 </template>
 
 <script>
@@ -20,6 +57,8 @@
     }),
     watch: {
       innerValue (newVal) {
+        console.log(newVal)
+        this.files = newVal
         this.$emit('input', {
           name: this.name,
           value: newVal,
@@ -27,6 +66,106 @@
       },
     },
     methods: {
+      onChange () {
+        this.$refs.file1.$refs.input.click()
+      },
+      sizeFormatter (num) {
+        // jacked from: https://github.com/sindresorhus/pretty-bytes
+        if (typeof num !== 'number' || isNaN(num)) {
+          throw new TypeError('Expected a number')
+        }
+
+        var exponent
+        var unit
+        var neg = num < 0
+        var units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+
+        if (neg) {
+          num = -num
+        }
+
+        if (num < 1) {
+          return (neg ? '-' : '') + num + ' B'
+        }
+
+        exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), units.length - 1)
+        num = (num / Math.pow(1000, exponent)).toFixed(2) * 1
+        unit = units[exponent]
+
+        return (neg ? '-' : '') + num + ' ' + unit
+      },
+      getIcon (key) {
+        const type = this.files[key].type ? this.files[key].type : this.files[key].mime_type
+        if (type) {
+          if (type.includes('video')) {
+            return { icon: 'mdi-play-circle-outline', color: 'black' }
+          }
+          if (type.includes('pdf')) {
+            return { icon: 'mdi-file-pdf-box', color: '#c11e07' }
+          }
+          if (type.includes('presentation')) {
+            return { icon: 'mdi-presentation', color: '#c24f32' }
+          }
+          if (type.includes('.document') || type.includes('msword')) {
+            return { icon: 'mdi-file-word-box', color: '#2372ba' }
+          }
+          if (type.includes('spreadsheetml')) {
+            return { icon: 'mdi-file-excel-box', color: '#30723f' }
+          }
+        }
+      },
     },
   }
 </script>
+<style scoped>
+.file-input>>>.v-input__prepend-outer{
+  margin: auto;
+  margin-bottom: 10px;
+}
+</style>
+<style lang="scss" scoped>
+.file-input{
+  background: #e2e2e205;
+  text-align: center;
+  width: 310px;
+  height: 100px;
+  border: 2px dashed #bbbbbb;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.files{
+  display: inline-grid;
+  grid-auto-flow: column;
+  grid-gap: 10px;
+  list-style: none;
+  padding: 0;
+  max-width: 100%;
+  overflow-x: auto;
+  margin-bottom: 10px;
+  &__single {
+    flex: 1 0 auto;
+    width: 150px;
+    min-height: 150px;
+    position: relative;
+    padding: 20px 10px 10px;
+    text-align: center;
+    color: white;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+
+    .close {
+      position: absolute;
+      right: 0;
+      top: 0;
+    }
+
+    i {
+      font-size: 50px;
+      display: block;
+      text-align: center;
+      margin-bottom: 10px;
+    }
+  }
+}
+</style>
